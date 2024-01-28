1612806d6465117c645b6c9d98a773d9afb60364
Lucas Satabin <lucas.satabin@gnieh.org>
2024-01-28 13:31:45 +0100

    Show border on vertical splits

--- neovim/plugins/nord.nix
+++ neovim/plugins/nord.nix
@@ -6,6 +6,7 @@
     extraLuaConfig = /* lua */ ''
       vim.g.nord_italic = true
       vim.g.nord_uniform_diff_background = true
+      vim.g.nord_borders = true
 
       require('nord').set()
     '';
