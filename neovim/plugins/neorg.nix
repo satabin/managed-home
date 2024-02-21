3b95e8f4c9a256d0cff26106bb18630e02b1497c
Lucas Satabin <lucas.satabin@gnieh.org>
2024-02-21 19:57:25 +0100

    Add neorg templates for daily work

--- neovim/plugins/neorg.nix
+++ neovim/plugins/neorg.nix
@@ -1,4 +1,10 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: let
+  fromGithub = import ../../utils/fromGithub.nix;
+in {
   programs.neovim = {
     plugins = with pkgs.vimPlugins; [
       (neorg.overrideAttrs ({postInstall ? "", ...}: {
@@ -9,6 +15,13 @@
           '';
       }))
       neorg-telescope
+      luasnip
+      (fromGithub {
+        owner = "pysan3";
+        repo = "neorg-templates";
+        rev = "v2.0.3";
+        hash = "sha256-nZOAxXSHTUDBpUBS/Esq5HHwEaTB01dI7x5CQFB3pcw=";
+      })
     ];
     extraLuaConfig =
       /*
@@ -20,6 +33,11 @@
             ["core.defaults"] = {},
             ["core.concealer"] = {},
             ["core.export"] = {},
+            ["core.esupports.metagen"] = {
+              config = {
+                timezone = "utc",
+              },
+            },
             ["core.dirman"] = {
               config = {
                 workspaces = {
@@ -34,9 +52,23 @@
               config = {
                 engine = "nvim-cmp",
               },
-            }
+            },
+            ["external.templates"] = {
+              config = {
+                keywords = {
+                  NOW = function()
+                    return require("luasnip").text_node(os.date("!%Y-%m-%dT%H:%M:%S+0000"))
+                  end,
+                },
+              },
+            },
           },
         }
       '';
+  };
+
+  xdg.configFile."nvim/templates/norg/" = {
+    source = ./neorg-templates;
+    recursive = true;
   };
 }
