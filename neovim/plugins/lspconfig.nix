834f53eb7511118957a7ab4952a9312351e22c74
Lucas Satabin <lucas.satabin@gnieh.org>
2024-07-01 19:22:57 +0200

    Add custom signs and hover styles

--- neovim/plugins/lspconfig.nix
+++ neovim/plugins/lspconfig.nix
@@ -1,8 +1,46 @@
+{ config, ... }:
 let
+  helpers = config.nixvim.helpers;
   options = { noremap = true; silent = true; };
+  signs = [
+    {
+      name = "DiagnosticSignError";
+      text = "";
+    }
+    {
+      name = "DiagnosticSignWarn";
+      text = "";
+    }
+    {
+      name = "DiagnosticSignHint";
+      text = "";
+    }
+    {
+      name = "DiagnosticSignInfo";
+      text = "";
+    }
+  ];
+  diagnostics = {
+    virtual_text = true;
+    signs = {
+      active.__raw = "signs";
+    };
+    update_in_insert = true;
+    underline = false;
+    severity_sort = true;
+    float = {
+      focusable = false;
+      style = "minimal";
+      border = "rounded";
+      source = "always";
+      header = "";
+      prefix = "";
+    };
+  };
 in
 {
   programs.nixvim = {
+
     plugins = {
       lsp = {
         enable = true;
@@ -20,6 +58,26 @@ in
         lightbulb.enable = false;
       };
     };
+
+    extraConfigLuaPre = /*lua*/''
+      local signs = ${helpers.toLuaObject signs}
+
+      for _, sign in ipairs(signs) do
+        vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
+      end
+
+      -- TODO: when diagnostics lands in 24.05, we can use it directly
+      vim.diagnostic.config(${helpers.toLuaObject diagnostics})
+
+      vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+        border = "rounded",
+      })
+
+      vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+        border = "rounded",
+      })
+    '';
+
 
     keymaps = [
       {
