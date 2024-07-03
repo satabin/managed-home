02a9ef3b70a1f2c5aee82df5ee0e9f1cabc60ded
Lucas Satabin <lucas.satabin@gnieh.org>
2024-07-03 10:49:32 +0200

    Use maintained lualine lsp progress plugin

--- neovim/plugins/lualine.nix
+++ neovim/plugins/lualine.nix
@@ -2,13 +2,53 @@
   programs.nixvim = {
     plugins.lualine = {
       enable = true;
+      iconsEnabled = true;
+      theme = "nord";
 
       sections.lualine_c = [
         "filename"
-        "lsp_progress"
+        {
+          name = {
+            __raw = /*lua*/''
+              function()
+                return require('lsp-progress').progress()
+              end
+            '';
+          };
+        }
       ];
     };
 
-    extraPlugins = [ pkgs.vimPlugins.lualine-lsp-progress ];
+    extraPlugins = [
+      (pkgs.vimUtils.buildVimPlugin {
+        name = "lsp-progress";
+        src = pkgs.fetchFromGitHub {
+          owner = "linrongbin16";
+          repo = "lsp-progress.nvim";
+          rev = "v1.0.12";
+          hash = "sha256-FmVGiaN5Rzvidt2HYe8uZTaShBWab/YPZuGAQHdMwLk=";
+        };
+      })
+    ];
+
+    extraConfigLua = /*lua*/ ''
+      require('lsp-progress').setup {}
+    '';
+
+    autoGroups = {
+      lualine_augroup = {
+        clear = true;
+      };
+    };
+
+    autoCmd = [
+      {
+        event = "User";
+        pattern = "LspProgressStatusUpdated";
+        callback = { __raw = "require('lualine').refresh"; };
+        group = "lualine_augroup";
+      }
+    ];
+
   };
 }
